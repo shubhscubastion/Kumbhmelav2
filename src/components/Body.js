8947ea22aa6d8c897bdc79b9f47859e99ec1282c
Shubh Sarpal <shubh.sarpal@cubastion.com>
2024-08-23 00:41:02 +0530

    Done

---
+++ src/components/Body.js
@@ -0,0 +1,8 @@
+function Body() {
+    return<div>
+        <p>This is the body for tent booking app</p>
+    </div>
+    
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+      <div className="headset">
+        <div className="logo">
+          <img src="https://www.thekumbhmelaindia.com/wp-content/themes/kumbhmelaindia-child/images/Exotic.png" alt="logo"></img>
+        </div>
+        <h1 style={{margin:10}}>Tent Booking </h1>
+      </div>
+    );
+}
+
+export default Header;
